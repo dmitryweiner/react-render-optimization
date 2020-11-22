2bac41072efda4aafedce26476939f5b609be01b
dmitryweiner <dmitry.weiner@gmail.com>
2020-11-22 14:02:33 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const MAX_ITEMS = 10000;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const arr = [];
+    for (let i = 0; i < MAX_ITEMS; i++) {
+        arr.push({
+            id: Math.random().toString(32).substr(2)
+        });
+    }
+    const [list, setList] = useState(arr);
+
+    function changeRandomItem() {
+        list[Math.floor(Math.random() * MAX_ITEMS)] = {
+            id: Math.random().toString(32).substr(2)
+        };
+        setList([...list]);
+    }
+
+    return (
+        <div className="App">
+            <button onClick={changeRandomItem}>Change random item</button>
+            <div className="columns-wrapper">
+                <div className="column">
+                    <ul>
+                        {list.map(item => (
+                            <li>{item.id}</li>
+                        ))}
+                    </ul>
+                </div>
+                <div className="column">
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                </div>
+                <div className="column">
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
